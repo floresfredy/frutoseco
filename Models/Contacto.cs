41dbc352a2b70b530a378b8c5fe69b4c7e97c555
Fred Duarte <fduartej@gmail.com>
2021-08-14 08:50:49 -0500

    First commit

---
+++ Models/Contacto.cs
@@ -0,0 +1,12 @@
+namespace demomvc.Models
+{
+    public class Contacto
+    {
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+
+        public string Nombre {get; set;}
+
+        public string Comentarios {get; set; }
+    }
+}
