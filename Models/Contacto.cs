31bd64bb2dfd562ebcce11ffd0db5036d0179145
Fred Duarte <fduartej@gmail.com>
2021-08-21 10:37:26 -0500

    Cambios en la entidad contacto

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -10,12 +10,17 @@ namespace demomvc.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("id")]
-        public string Id { get; set; }
+        public int Id { get; set; }
+        [Column("email")]
         public string Email { get; set; }
+        [Column("phone")]
         public string Phone { get; set; }
+        [Column("name")]
         public string Name {get; set;}
-        public string Comment {get; set; }
+        [Column("comment")]
+        public string Comment {get; set; }     
         public DateTime BirthDate { get; set; }
+        [Column("gender")]
         public string Gender { get; set; }
 
     }
