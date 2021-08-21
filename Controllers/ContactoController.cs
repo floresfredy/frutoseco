c4f8c78feee393c56bee6f2e4f1e98f7280241d7
Fred Duarte <fduartej@gmail.com>
2021-08-21 10:24:58 -0500

    Grabar Contacto

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -1,19 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using demomvc.Models;
+using demomvc.Data;
 
 namespace demomvc.Controllers
 {
     public class ContactoController:Controller
     {
-        
+        private readonly ApplicationDbContext _context;
+
+        public ContactoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Create()
+        public IActionResult Create(Contacto objContacto)
         {
-            
+            _context.Add(objContacto);
+            _context.SaveChanges();
             ViewData["Message"] = "El contacto ya esta registrado";
             return View("Index");
         }
--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -1,8 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 namespace demomvc.Models
 {
+    [Table("t_contacto")]
     public class Contacto
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
         public string Id { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
